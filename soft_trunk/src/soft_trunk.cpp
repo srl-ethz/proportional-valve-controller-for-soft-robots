76045850d29b6d0289f62ea37c5246542be9603e
Robert Katzschmann <rkk@csail.mit.edu>
2018-09-12 10:51:57 -0400

    testing: working on linux with actual valve array from festo

--- soft_trunk/src/soft_trunk.cpp
+++ soft_trunk/src/soft_trunk.cpp
@@ -6,8 +6,10 @@
 
 int main() {
 	// Create MPA controller.
-	MPA mpa("127.0.0.1", "502");
-
+	MPA mpa("192.168.1.101", "502");
+	unsigned int valveNum = 0; //test valves 0 to 15
+	unsigned int startPressure = 400; //tested 0 to 1000 mbar
+	unsigned int endPressure = 0;
 	// Connect.
 	if (!mpa.connect()) {
 		std::cout << "Failed to connect to MPA." << std::endl;
@@ -15,26 +17,28 @@ int main() {
 	}
 
 	// Set valve 0 to 1 bar.
-	mpa.set_single_pressure(0, 1000);
+	mpa.set_single_pressure(valveNum, startPressure);
 
-	// Wait 100 ms.
-	std::this_thread::sleep_for(std::chrono::milliseconds(100));
+	for ( int i=0; i<5; i++) {
+		// Wait 100 ms.
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
-	// Read pressure of valve 0.
-	std::cout << "Valve 0: "
-		<< mpa.get_single_pressure(0)
-		<< " mbar"
-		<< std::endl;
-
+		// Read pressure of valve 0.
+		std::cout << "Valve " << valveNum << " :"
+			<< mpa.get_single_pressure(valveNum)
+			<< " mbar"
+			<< std::endl;
+	}
+	
 	// Set valve 0 to 0 bar (off).
-	mpa.set_single_pressure(0, 0);
+	mpa.set_single_pressure(valveNum, endPressure);
 
 	// Wait 100 ms.
-	std::this_thread::sleep_for(std::chrono::milliseconds(100));
+	std::this_thread::sleep_for(std::chrono::milliseconds(500));
 
 	// Read pressure of valve 0.
-	std::cout << "Valve 0: "
-		<< mpa.get_single_pressure(0)
+	std::cout << "Valve " << valveNum << " :"
+		<< mpa.get_single_pressure(valveNum)
 		<< " mbar"
 		<< std::endl;
 
